ff4deb8e3b48a8710c86d8ca941a2b337218ef95
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-25 02:13:56 +0530

    added template generator

---
+++ genTemplate.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+Title = input("Enter the blog title here : ")
+TitleWords = Title.split()
+Title = "_draft_" + "-".join(TitleWords)
+
+os.mkdir(path="_posts/"+Title)
+date = time.localtime()
+fileName = f"{date.tm_year}-{date.tm_mon}-{date.tm_mday}-{Title}.md"
+f = open(f"_posts/{Title}/{fileName}", "w")
+f.writelines([
+    "---\n",
+    f"title: {' '.join(TitleWords)}\n",
+    f"date: {date.tm_year}-{date.tm_mon}-{date.tm_mday} {date.tm_hour}:{date.tm_min}:{date.tm_sec} {time.timezone}\n",
+    "tags: []\n",
+    f"description: {input('Enter Blog Description : ')}\n",
+    "---\n",
+])
+f.close()
